e4bdb2ab87962344c3be53cc21c1d339c79cc925
220106 <220106rq@yse-c.net>
2024-01-19 11:08:26 +0900

    new

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\TweetController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RegistUserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//ルーティングのURL：「/api/xxxx」
+// TweetController
+Route::get('/tweet/get', [TweetController::class, 'get']);
+Route::post('/tweet/add', [TweetController::class, 'add']);
+
+Route::post('/regist/store', [RegistUserController::class, 'store']);
+// AuthController
+Route::post('/auth', [AuthController::class, 'auth']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
